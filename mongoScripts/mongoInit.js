00bce73c5db0ce6ae0104f8869103e030db801aa
ischoe <schoenberger.ingo@gmail.com>
2017-01-06 09:23:39 +0100

    added new users and routines

--- mongoScripts/mongoInit.js
+++ mongoScripts/mongoInit.js
@@ -8,9 +8,12 @@ const MongoClient = mongodb.MongoClient,
 
 const insertUsers = function(db, callback) {
    db.collection('users').insertMany([
- 			{"id" : 1, "firstName" : "Bob", "lastName" : "Smith", "email" : "bob@gmx.com"},
- 			{"id" : 2, "firstName" : "Ben", "lastName" : "Smooth", "email" : "ben@gmx.com"},
- 			{"id" : 3, "firstName" : "Benjamin", "lastName" : "Smatch", "email" : "benjamin@gmx.com"}
+ 			{"_id" : 1, "firstName" : "Holger", "email" : "", "key": "12345", "token": "", "role": "user"},
+ 			{"_id" : 2, "firstName" : "Julia", "email" : "", "key": "12345", "token": "", "role": "user"},
+ 			{"_id" : 3, "firstName" : "Michael", "email" : "", "key": "34567", "token": "", "role": "user"},
+      {"_id" : 4, "firstName" : "Werner", "email" : "", "key": "123", "token": "", "role": "user"},
+ 			{"_id" : 5, "firstName" : "Elisabeth", "email" : "", "key": "123", "token": "", "role": "user"},
+      {"_id" : 6, "firstName" : "Admin", "email" : "", "key": "supersecret1337", "token": "", "role": "admin"}
  		], function(err, result) {
     assert.equal(err, null);
     console.log("Inserted a document into the users collection.");
@@ -18,11 +21,19 @@ const insertUsers = function(db, callback) {
   });
 };
 
+function createAdmin(db) {
+  //db.removeUser(config.mongoAdmin);
+  //db.addUser(config.mongoAdmin, config.mongoPassword);
+  //console.log('created Admin');
+}
+
 mongoClean(function(){
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server.");
   	insertUsers(db, function() {
+        console.log('inserted Users');
+        createAdmin(db);
         db.close();
     });
   });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import assert from 'assert';
 import mongodb from 'mongodb';
 import config from '../config';
+import bodyParser from 'body-parser';
+import uuid from 'uuid/v1';
+import cors from 'cors';
 
 const port = config.port,
 	app = express(),
@@ -12,27 +15,138 @@ const getUsers = function(db, callback) {
 	 var collection = db.collection('users');
 	 collection.find({}).toArray(function(err, docs) {
     assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
     callback(docs);
   });
 };
 
-app.set('port', port);
+const getUsersByKey = function(db, sendKey, callback) {
+	 var collection = db.collection('users');
+	 collection.find({key:sendKey}).toArray(function(err, docs) {
+			 assert.equal(err, null);
+			 if(docs) {
+		     callback(docs);
+			 } else {
+				 callback(null);
+			 }
+   });
+};
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const getUsersByToken = function(db, token, callback) {
+	 var collection = db.collection('users');
+	 collection.find({token:token}).toArray(function(err, docs) {
+			 assert.equal(err, null);
+			 if(docs) {
+		     callback(docs);
+			 } else {
+				 callback(null);
+			 }
+   });
+};
+
+const getUserByTokenAndId = function(db, token, id, callback) {
+	 var collection = db.collection('users');
+	 collection.find({token:token, _id: id}).toArray(function(err, docs) {
+			 assert.equal(err, null);
+			 if(docs) {
+		     callback(docs);
+			 } else {
+				 callback(null);
+			 }
+   });
+};
+
+const updateUsersByKey = function(db, sendKey, sessionId, callback) {
+	 var collection = db.collection('users');
+		collection.update(
+			{key:sendKey},
+			{$set:{ token: sessionId} },
+			{ multi: true },
+		function(err, writeResult) {
+			 assert.equal(err, null);
+			 if(writeResult) {
+				 callback(writeResult);
+			 } else {
+				 callback(null);
+			 }
+   });
+};
+
+const updateUsersByProperty = function(db, id, prop, val, callback) {
+	 var collection = db.collection('users'),
+	 	set = { [prop] : val};
+		collection.update(
+			{_id:id},
+			{$set: set },
+			{ multi: false },
+		function(err, writeResult) {
+			 assert.equal(err, null);
+			 if(writeResult) {
+				 callback(writeResult);
+			 } else {
+				 callback(null);
+			 }
+   });
+};
+
+app.set('port', port);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(securityCheck);
+
+function securityCheck( req, res, next){
+	if(req.get('origin') === config.allowedUrl) {
+		next();
+	} else {
+		res.status(404).send('Not found');
+	}
+}
+
+function isLoggedIn(req, res, next) {
+	if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+		const token = req.headers.authorization.split(' ')[1];
+		MongoClient.connect(mongoUrl, function(err, db) {
+			assert.equal(null, err);
+			getUsersByToken(db, token, function(data) {
+				if(data.length > 0) {
+					console.log('USER IS LOGGED-IN');
+					next();
+				} else {
+					res.status(404).send('Bad login');
+				}
+				db.close();
+			});
+		});
+	} else {
+		res.status(404).send('Bad login');
+	}
+}
+
+function isAdmin(req, res, next) {
+	if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+		const token = req.headers.authorization.split(' ')[1];
+		MongoClient.connect(mongoUrl, function(err, db) {
+			assert.equal(null, err);
+			getUsersByToken(db, token, function(data) {
+				if(data.length > 0 && data[0].role === 'admin') {
+					console.log('USER IS ADMIN');
+					next();
+				} else {
+					res.status(404).send('Bad login');
+				}
+				db.close();
+			});
+		});
+	} else {
+		res.status(404).send('Bad login');
+	}
+}
 
 app.get('/', function(req, res) {
 		res.send('it´s working');
 });
 
-app.get('/users', function(req, res){
+app.get('/users', isAdmin, function(req, res){
 	MongoClient.connect(mongoUrl, function(err, db) {
 		assert.equal(null, err);
 		getUsers(db, function(data) {
@@ -50,6 +164,96 @@ app.get('/demo', function(req, res) {
 			{"id" : 3, "firstName" : "Benjamin", "lastName" : "Smatch", "email" : "benjamin@gmx.com"}
 		]);
 });
+
+app.post('/loginUser', securityCheck, function (req, res) {
+	const sessionId = uuid();
+	if(res.statusCode === 200) {
+		if(req.body.data.key.length > 0) {
+			MongoClient.connect(mongoUrl, function(err, db) {
+				assert.equal(null, err);
+				updateUsersByKey(db, req.body.data.key, sessionId, function(data) {
+					if(data && data.result.nModified > 0) {
+						getUsersByKey(db, req.body.data.key, function(data) {
+							res.json(Object.assign({}, {user:data}, {status:true}));
+							res.end();
+							db.close();
+						});
+					} else {
+						res.json({
+							status : false
+						});
+						res.end();
+						db.close();
+					}
+				});
+			});
+		} else {
+			res.json(Object.assign({}, {status:false}));
+			res.end();
+		}
+	} else {
+		res.status(404).end();
+	}
+});
+
+app.post('/isValidKey', function (req, res) {
+	MongoClient.connect(mongoUrl, function(err, db) {
+		assert.equal(null, err);
+		getUsersByToken(db, req.body.data.token, function(data) {
+			if(data.length > 0) {
+				res.json(Object.assign({}, {user:data}, {status:true}));
+			} else {
+				res.json({
+					status : false
+				});
+			}
+			res.end();
+			db.close();
+		});
+	});
+});
+
+
+app.post('/updateUserData', function (req, res) {
+	const token = req.body.data.token,
+			id = parseInt(req.body.data.id);
+	MongoClient.connect(mongoUrl, function(err, db) {
+		assert.equal(null, err);
+		getUserByTokenAndId(db, token, id, function(data) {
+			if(data.length > 0) {
+				updateUsersByProperty(db, id, req.body.data.prop, req.body.data.val, function(data) {
+					if(data && data.result.nModified > 0) {
+						getUsersByToken(db, token, function(data) {
+							res.json(Object.assign({}, {user:data}, {status:true}));
+							res.end();
+							db.close();
+						});
+					} else if(data && data.result.nModified === 0){
+						getUsersByToken(db, token, function(data) {
+							res.json(Object.assign({}, {user:data}, {status:true}));
+							res.end();
+							db.close();
+						});
+					} else {
+						res.json({
+							status : false
+						});
+						res.end();
+						db.close();
+					}
+				});
+
+			} else {
+				res.json({
+					status : false
+				});
+				res.end();
+				db.close();
+			}
+		});
+	});
+});
+
 
 app.listen(port, function() {
 	console.log("API running on port:" + app.get('port'));
